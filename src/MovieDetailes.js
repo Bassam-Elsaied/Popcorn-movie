3f888d4753daa20fce3e0ea5770a1f01c539ccc7
Bassam Elsayed <bassame993@gmail.com>
2024-10-10 09:38:00 +0300

    fixing fetch problem

--- src/MovieDetailes.js
+++ src/MovieDetailes.js
@@ -51,7 +51,7 @@ export default function MovieDetailes({
     async function fetchMovieDetails() {
       setIsLoading(true);
       const res = await fetch(
-        `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+        `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
       );
       const data = await res.json();
       setMovie(data);
